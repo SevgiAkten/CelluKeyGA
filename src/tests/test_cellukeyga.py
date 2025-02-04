f7e593ef461e54df7c70f4554332b0e529c0381f
SevgiAkten <sevgiakten@gmail.com>
2025-02-04 13:33:17 +0300

    edit c

--- src/tests/test_cellukeyga.py
+++ src/tests/test_cellukeyga.py
@@ -2,9 +2,9 @@ import pytest
 from cellukeyga import add_two_digits
 
 def test_add_two_digits_valid():
-    assert add_two_digits(23, 45) == 68
-    assert add_two_digits(0, 99) == 99
-    assert add_two_digits(50, 50) == 100
+    assert add_two_digits(2, 4) == 6
+    assert add_two_digits(0, 9) == 9
+    assert add_two_digits(5, 5) == 10
 
 def test_add_two_digits_invalid():
     with pytest.raises(ValueError):
